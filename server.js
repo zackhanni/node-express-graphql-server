cc7a8ff8d7f4fdc64b1b4903ef9b14b5225583b9
Zack Hanni <zack.hannI@gmail.com>
2025-07-27 17:28:01 -0400

    created addAuthor mutation

--- server.js
+++ server.js
@@ -99,6 +99,21 @@ const RootMutationType = new GraphQLObjectType({
         return book;
       },
     },
+    addAuthor: {
+      type: AuthorType,
+      description: "Add an author",
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parent, args) => {
+        const author = {
+          id: authors.length + 1,
+          name: args.name,
+        };
+        authors.push(author);
+        return author;
+      },
+    },
   }),
 });
 
